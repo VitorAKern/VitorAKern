b72fa58048b9e2d2f78af57fdd768214eb10a515
Vitor Kern <vitor.kern@totvs.com.br>
2018-08-20 14:03:00 -0300

    Mudanças Logo e Layout
    Mudanças Logo e Layout

--- JS/Navigation.js
+++ JS/Navigation.js
@@ -1,6 +1,7 @@
 $(document).ready(function($) {
     $('li').click(function() {
       $(this).addClass('active').siblings('li').removeClass('active');
+      if($('#logo').hasClass('active')) { $('#logo').removeClass('active'); }
       $('section:nth-of-type('+$(this).data('rel')+')').stop().fadeIn(400, 'linear').siblings('section').stop().fadeOut(400, 'linear'); 
     });
   })(jQuery);
